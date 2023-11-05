6b0f4dc1bc3c99b143a075803c6729bd09efe8f9
Linus Karlsson <linus@zozs.se>
2023-11-05 17:09:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Melodikrysset 2x2
+
+En hemsida som varje vecka genererar en 2x2 plan på en A4-sida med de fyra nästkommande veckornas [Melodikryss](https://sverigesradio.se/melodikrysset).
+
+Hämtar melodikryssen från [SR:s sida med högupplösta kryss](https://sverigesradio.se/artikel/754398), men kombinerar flera veckor på ett A4.
+
+## Licens
+
+Mjukvaran är licensierad under GNU AGPL v3 eller senare. Se filen `LICENSE` för fullständig text.
+
+```
+melodikrysset2x2
+Copyright (c) 2023, Linus Karlsson
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+```
---
+++ create4.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+# vim: ts=4:sw=4:expandtab
+
+use v5.34;
+use Cwd;
+use DateTime;
+use File::Copy;
+use File::Temp ();
+
+# Generate array with "2023w44 2023w45 2023w46" etc.
+my @weeks;
+my $dt = DateTime->now();
+for (my $i = 0; $i < 4; $i++) {
+    push @weeks, $dt->strftime("%Yw%V");
+    $dt->add(weeks => 1);
+}
+
+# Create temporary directory for data
+my $dir = File::Temp->newdir;
+my $old_cwd = getcwd();
+chdir($dir);
+
+# Download corresponding crosswords
+my @latex_strings;
+foreach my $week (@weeks) {
+    my $file = "kryss$week";
+    push @latex_strings, "$file,1";
+    my $url = "https://sr.korsord.se/images/kryss/$file.pdf";
+    system("curl", "-OL", $url);
+}
+
+# Prepare LaTeX source with crosswords
+my $crosswords = join ",", @latex_strings;
+open(my $fh, ">", "melkryss.tex") or die("Failed to open file");
+print $fh <<'END';
+\documentclass[a4paper]{article}
+\usepackage[margin=5mm,nohead,nofoot]{geometry}
+\usepackage{pdfpages}
+\pagestyle{empty}
+\parindent0pt
+\begin{document}
+END
+print $fh "\\includepdfmerge[nup=2x2]{$crosswords}\n";
+print $fh "\\end{document}\n";
+close($fh);
+
+system("pdflatex", "melkryss.tex");
+copy("melkryss.pdf", "$old_cwd/melkryss.pdf");
+
+# Go back to old working directory (this allows temp dir to be cleaned up)
+chdir($old_cwd);
+
